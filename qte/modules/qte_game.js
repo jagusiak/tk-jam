b7e8e31b25d88baa35c4ef60da6637ef8fc306ea
Seweryn Jagusiak <seweryn.jagusiak@sofomo.eu>
2016-02-27 22:36:45 +0100

    hej, i co mi zrobisz?

--- qte/modules/qte_game.js
+++ qte/modules/qte_game.js
@@ -6,7 +6,7 @@ window.SJ.module('qte_game', function (sj) {
             jumpAnimation = sj.animation.create(guy);
             slideAnimation = sj.animation.create(guy);
             stinkAnimation = sj.animation.create(stink);
-            jumpAnimation.setStep(5);
+            jumpAnimation.setStep(6);
             jumpAnimation.setLooped(false);
             stinkAnimation.setStep(4);
             slideAnimation.setStep(5);
@@ -31,15 +31,27 @@ window.SJ.module('qte_game', function (sj) {
                 frame = 0, phase_counter = 0,
                 letters, letter, letterObjects = [],
                 listener,
-                generator, generated, progress;
+                generator, generated, progress, generateObstacle = true, obstacles = [],
+                downObstacles = ['ob_0', 'ob_1', 'ob_2', 'ob_3'], upObstacles = [], j = 0, currentObstacle, done = false;
             canvas = sj.canvas;
             canvas.init();
             scene = canvas.createScene('scene_1', sj.config('scenes', 'run'));
             background = scene.getObject('background');
-            obstacle = scene.getObject('tree');
-            obstacle.setPosition(-1, obstacle.y, obstacle.z);
+
             progress = scene.getObject("progress");
             guy = scene.getObject('guy');
+
+            for (var n in downObstacles) {
+                obstacles[j] = scene.getObject(downObstacles[n]);
+                obstacles[j]['animation'] = jumpAnimation;
+                j++;
+            }
+
+            for (n in upObstacles) {
+                obstacles[j] = scene.getObject(upObstacles[n]);
+                obstacles[j]['animation'] = slideAnimationion;
+                j++;
+            }
 
             generator = sj.qte_generator;
             generated = generator.next();
@@ -54,8 +66,8 @@ window.SJ.module('qte_game', function (sj) {
             currentAnimation = runAnimation;
 
             scene.onFrame = function () {
-                var leftSeconds = 10 - Math.floor(frame/25);
-                frame++, phase_counter++;
+                var leftSeconds = 10 - Math.floor(frame/25), select;
+                frame++; phase_counter++;
 
                 progress.setTexture(progress.texture, 0, 0, leftSeconds/10,1);
                 progress.setPosition(0.02*leftSeconds + 0.03, 0.03, 10);
@@ -68,58 +80,105 @@ window.SJ.module('qte_game', function (sj) {
                     return;
                 }
 
+                if (generateObstacle) {
+                    select = Math.floor(Math.random()*obstacles.length);
+                    while (obstacles[select].visible) {
+                        select = Math.floor(Math.random()*obstacles.length);
+                    }
+                    var o = obstacles[select];
+                    o.setPosition(frame < 5 ? -0.7 : -0.4, o.y, o.z);
+                    o.setVisible(true);
+                    generateObstacle = false;
+                }
 
                 background.setPosition(currentLocation - Math.floor(currentLocation / 1.5) * 1.5, background.y, background.z);
                 currentAnimation.play();
                 if (currentAnimation.hasStopped()) {
                     currentAnimation = runAnimation;
                 }
-                if (obstacle.x > 0.2 && obstacle.x < 0.5) {
-                    speed = 0.05;
-                    runAnimation.setStep(1);
-                } else {
-                    if (speed > 0.02) {
-                        currentAnimation = jumpAnimation;
-                        currentAnimation.setCurrentFrame(0);
+
+
+                // if (obstacle.x > 0.2 && obstacle.x < 0.5) {
+                //     speed = 0.05;
+                //     runAnimation.setStep(1);
+                // } else {
+                //     if (speed > 0.02) {
+                //         currentAnimation = jumpAnimation;
+                //         currentAnimation.setCurrentFrame(0);
+                //     }
+                //     speed = 0.02;
+                //     runAnimation.setStep(2);
+                // }
+                currentLocation += speed;
+
+
+                for (select in obstacles) {
+                    var ob = obstacles[select];
+                    if (ob.visible) {
+                        ob.setPosition(ob.x + speed, ob.y, ob.z);
+                        if (ob.x > 1.8) {
+                            ob.setVisible(false);
+                        }
+                        if (ob.x > -0.5 && ob.x < 0.50 && !currentObstacle) {
+                            currentObstacle = ob;
+                            phase_counter = 0;
+
+                            listener.clear();
+                            generated = generator.next();
+
+                            for (var obj in sj.config('keys', 'keys')) {
+                                var ind = generated.keys.indexOf(obj), letter = letterObjects[obj];
+                                if (ind != -1) {
+                                    letters.position(letter, ind);
+
+                                    letter.setVisible(true);
+                                } else {
+                                    letter.setVisible(false);
+                                }
+                            }
+                            speed = 0.02;
+                            done = false;
+
+
+                        }
+                        if (currentObstacle && currentObstacle.x > 0.50) {
+                            currentObstacle = undefined;
+                            generateObstacle = true;
+                            if (done) {
+                                currentAnimation = jumpAnimation;
+                                currentAnimation.setCurrentFrame(0);
+                            } else {
+                                // wyłącz
+                            }
+                            listener.clear();
+                        }
                     }
-                    speed = 0.02;
-                    runAnimation.setStep(2);
-                }
-                currentLocation += speed + speed_add;
-                obstacle.setPosition(obstacle.x + speed + speed_add, obstacle.y, obstacle.z);
 
-                if (obstacle.x > 1.8) {
-                    obstacle.setPosition(-0.5, obstacle.y, obstacle.z);
                 }
 
+                if (done) {
+                    speed = 0.06;
+                }
+                //obstacle.setPosition(obstacle.x + speed + speed_add, obstacle.y, obstacle.z);
+
+                // if (obstacle.x > 1.8) {
+                //     obstacle.setPosition(-0.5, obstacle.y, obstacle.z);
+                // }
+                //
+
+                if (currentObstacle) {
                 for (var obj in generated.keys) {
                     var ident = generated.keys[obj], check = listener.check(generated, ident);
 
                     letters.state(letterObjects[ident], check);
 
                     if(sj.letters.STATE_CORRECT === check){
-                        speed_add = 0.075;
+                         done = true;
+                         listener.clear();
                     }
                 }
-
-                if (phase_counter % (60 - (0 < speed_add ? 30 : 0)) === 0) {
-                    speed_add = 0;
-                    phase_counter = 0;
-
-                    listener.clear();
-                    generated = generator.next();
-
-                    for (var obj in sj.config('keys', 'keys')) {
-                        var ind = generated.keys.indexOf(obj), letter = letterObjects[obj];
-                        if (ind != -1) {
-                            letters.position(letter, ind);
-
-                            letter.setVisible(true);
-                        } else {
-                            letter.setVisible(false);
-                        }
-                    }
                 }
+
             };
 
             sj.input.onKeyDown(function (key) {
