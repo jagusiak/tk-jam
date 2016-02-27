cab35c9551f226ce453ce483706df0e5ce1f9ef2
lukasz.modlinski <lukasz.modlinski@gmail.com>
2016-02-27 11:06:19 +0100

    generator with parts

---
+++ qte/modules/combos/random.js
@@ -0,0 +1,25 @@
+window.SJ.module('random', function(sj) {
+    return {
+        'get' : function () {
+            var keys = sj.config('keys', 'keys'),
+                distrib = {},
+                rand, selected,
+                sum = 0;
+
+            for (var i in keys) {
+                sum += parseFloat(keys[i]);
+                distrib[i] = sum;
+            }
+
+            rand = Math.random() * sum;
+
+            for (var j in distrib) {
+                if (distrib[j] >= rand) {
+                    return j;
+                }
+            }
+
+            debugger;
+        }
+    };
+});
--- qte/modules/qte_generator.js
+++ qte/modules/qte_generator.js
@@ -1,9 +1,10 @@
 window.SJ.module('qte_generator', function(sj) {
     return {
         'next' : function() {
-            var types = [sj.single, sj.pair, sj.choice, sj.sequence];
-            return types[types.length * Math.random() << 0]
+            var types = [sj.single, sj.pair, sj.choice, sj.sequence, sj.triple],
+            random = types[types.length * Math.random() << 0];
+
+            return random.next();
         }
     };
-
 });
